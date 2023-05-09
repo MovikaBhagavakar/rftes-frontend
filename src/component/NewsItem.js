e2f97e80e931bb9dda3bbde7b65652f89bf4804a
Kamal Sharma <kamal5201ks@gmail.com>
2023-05-09 21:12:38 +0530

    Added Add to fav.

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,29 +2,66 @@ import React, { useEffect, useState } from 'react'
 
 const NewsItem = (props) => {
   const [active, setActive] = useState(false)
-  const [userData, setUserData] = useState(null)
   //  add props
   let { title, description, imgurl, nwesurl, author, date, source, id } = props;
 
-  useEffect(()=>{
-    fetch(`http://localhost:8080/users/${JSON.parse(localStorage.getItem("rftes")).userExist._id}`, {
-      method:"GET"
-    }).then((res) => res.json()).then((data) => setUserData(data.favourites))
-  },[])
-  
-  console.log(userData)
+  useEffect(() => {
+    fetch(`http://localhost:8080/v1/users/${JSON.parse(localStorage.getItem("rftes")).userExist._id}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
+      }
+    }).then((res) => res.json()).then((data) => props.setUserData(data.responseData.favourites))
+  }, [])
+
+  useEffect(() => {
+    if (props.userData?.includes(id)) {
+      setActive(true)
+    } else {
+      setActive(false)
+    }
+  }, [props.userData])
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/v1/users/${JSON.parse(localStorage.getItem("rftes")).userExist._id}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
+      }
+    }).then((res) => res.json()).then((data) => props.setUserData(data.responseData.favourites))
+  }, [active])
 
   const handleLike = () => {
-    fetch("http://localhost:8080/v1/users/", {
-      method:"POST",
-      body:JSON.stringify({
-        favourites:[id]
-      }),
-      headers:{
-        "Content-Type":"application/json",
-        "Authorization":`Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
-      }
-    })
+    if (props.userData?.includes(id)) {
+      const index = props.userData?.indexOf(id);
+      const updatedState = props.userData?.splice(index, 1)
+      props.setUserData(updatedState)
+
+      fetch(`http://localhost:8080/v1/users/${JSON.parse(localStorage.getItem("rftes")).userExist._id}`, {
+        method: "POST",
+        body: JSON.stringify({
+          favourites: props.userData
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
+        }
+      })
+    } else {
+      const updatedState = [...props.userData, id]
+      props.setUserData(updatedState)
+      console.log(props.userData)
+      fetch(`http://localhost:8080/v1/users/${JSON.parse(localStorage.getItem("rftes")).userExist._id}`, {
+        method: "POST",
+        body: JSON.stringify({
+          favourites: updatedState
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
+        }
+      })
+    }
   }
 
   return (
@@ -34,14 +71,14 @@ const NewsItem = (props) => {
         {/* showing top titles  */}
         <span style={{ left: "10%", textAlign: "end" }} className="position-absolute top=0 start=100  translate-middle badge rounded-pill bg-danger">
           {source}</span>
-      {
-        id && <div className={`p-2 position-absolute ${active && "bg-danger"}`} style={{borderRadius:"50%"}}>
-          <i onClick={()=>{
-            setActive(!active)
-            handleLike()
-          }} className="fa-regular fa-heart text-white" style={{cursor:"pointer"}}></i>
-        </div>
-      }
+        {
+          id && <div className={`p-2 position-absolute ${active && "bg-danger"}`} style={{ borderRadius: "50%" }}>
+            <i onClick={() => {
+              setActive(!active)
+              handleLike()
+            }} className="fa-regular fa-heart text-white" style={{ cursor: "pointer" }}></i>
+          </div>
+        }
         {/* if image is null then this image will show */}
         <img src={imgurl ? imgurl : "https:static.toiimg.com/thumb/msid-93287340,width-1070,height-580,imgsize-221349,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg "} className="card-img-top" alt="img" />
         <h5 className="card-title">{title}...</h5>
--- src/component/SubNews.js
+++ src/component/SubNews.js
@@ -7,43 +7,46 @@ import { wrapper } from "../App";
 
 export default function SubNews(props) {
   const [articles, setArticles] = useState([])
-  useEffect(()=>{
+  const [userData, setUserData] = useState(null)
+  useEffect(() => {
     fetch(`http://localhost:8080/v1/articles?category=${props.category}`, {
-      method:"GET",
-      headers:{
-        "Content-Type":"application/json",
-        "authorization":`Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": `Bearer ${JSON.parse(localStorage.getItem("rftes")).token}`
       }
-    }).then((res)=>res.json()).then((data)=> {console.log(data); setArticles(data.responseData)}).catch((err) => console.log(err.message))
-  },[])
+    }).then((res) => res.json()).then((data) => { console.log(data); setArticles(data.responseData) }).catch((err) => console.log(err.message))
+  }, [])
   return (
     <>
-    {/* title and category pass  */}
+      {/* title and category pass  */}
       {/* <h1 className="text-center " style={{ marginTop: "150px" }}>RFTES-Top {props.category} Headline</h1> */}
       {/* //infinite scroll use for infinite scrolling */}
-        <div className="container">
-          <div className="row">
-            {articles.map((element) => {
-              console.log(element)
-              return (
-                <div className="col-md-4" key={element?.url}>
-                  <NewsItem
+      <div className="container">
+        <div className="row">
+          {articles.map((element) => {
+            console.log(element)
+            return (
+              <div className="col-md-4" key={element?.url}>
+                <NewsItem
                   //titles came in such a size that's why used slices
-                    title={element?.heading ? element.heading.slice(0, 45) : ""}
-                    description={
-                      element?.description ? element.description.slice(0, 88) : ""}
-                    author={element?.author || "Anonymous"} 
-                    date={element?.createdAt} 
-                    source={element?.source?.name || ""} 
-                    imgurl={element?.urlToImage || ""}  
-                    nwesurl={element?.url || ""}
-                    id={element?._id}
-                  />
-                </div>
-              );
-            })}
-          </div>
+                  title={element?.heading ? element.heading.slice(0, 45) : ""}
+                  description={
+                    element?.description ? element.description.slice(0, 88) : ""}
+                  author={element?.author || "Anonymous"}
+                  date={element?.createdAt}
+                  source={element?.source?.name || ""}
+                  imgurl={element?.urlToImage || ""}
+                  nwesurl={element?.url || ""}
+                  id={element?._id}
+                  userData={userData}
+                  setUserData={setUserData}
+                />
+              </div>
+            );
+          })}
         </div>
+      </div>
     </>
   );
 
